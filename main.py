0e8ac8f5d66ef5583d8928728d3476c58da75b8c
SuperYummy <75013273+SuperYummy@users.noreply.github.com>
2021-02-17 07:11:58 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,85 @@
+# https://discord.com/oauth2/authorize?client_id=782990263391092776&scope=bot&permissions=8
+import discord
+from discord.ext import commands
+import logging
+import random
+import os
+
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+
+with open("prefix.0", "r", encoding="utf-8") as f:
+    prefix = f.read()
+
+client = commands.Bot(command_prefix =prefix)
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('Danganronpa V2: Bloodlust'))
+    print('We have logged in as {0.user}'.format(client))
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+
+
+@client.command()
+async def meow(ctx):
+    await ctx.send("Meow!")
+
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ['Yes', 'No', 'I don\'t know']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'Banned {member.mention}')
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+        
+        if (user.name, user.discrimator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+
+with open("token.0", "r", encoding="utf-8") as f:
+    token = f.read()
+
+client.run(token)
+
